63bee9939d0b75e49332a555cc8384aa59f0a5f8
pujuntao <pujuntao@zile.ai>
2019-06-09 03:26:22 +0800

    create baseActivity & baseApplication

---
+++ base/src/main/java/cn/pjt/base/BaseActivity.java
@@ -0,0 +1,45 @@
+package cn.pjt.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ base/src/main/java/cn/pjt/base/BaseApplication.java
@@ -0,0 +1,12 @@
+package cn.pjt.base;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
